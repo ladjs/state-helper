ade55c3bbb0a7c7ab6c94f6450907ace52dd2c4a
titanism <101466223+titanism@users.noreply.github.com>
2022-07-21 01:04:43 -0500

    feat: do not cache flash messages (prevents rendering twice on back button)

--- index.js
+++ index.js
@@ -17,10 +17,26 @@ class StateHelpers {
         !_.isObject(ctx.session.flash)
       )
         return {};
-      return _.zipObject(
+      const obj = _.zipObject(
         _.keys(ctx.session.flash),
         _.keys(ctx.session.flash).map((key) => ctx.flash(key))
       );
+      //
+      // set no cache to prevent the same messages from rendering on back button
+      // <https://github.com/helmetjs/nocache/blob/6de09b822c6e0cd95655d2b454ffa2659338d995/index.js#L3-L9>
+      //
+      if (
+        !_.isEmpty(obj) &&
+        !Object.keys(obj).every((key) => _.isEmpty(obj[key]))
+      ) {
+        ctx.set('Surrogate-Control', 'no-store');
+        ctx.set(
+          'Cache-Control',
+          'no-store, no-cache, must-revalidate, proxy-revalidate'
+        );
+        ctx.set('Pragma', 'no-cache');
+        ctx.set('Expires', '0');
+      }
     };
 
     return next();
